4c49f3415d4cf3d1529059a417cc5993d790fd86
Kennedy Njeri <mistakenz123@gmail.com>
2019-03-03 20:58:32 +0300

    Added url tests

--- mentee/tests.py
+++ mentee/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.shortcuts import reverse
+from .models import User
 
 # Create your tests here.
+
+
+class HomePageTests(SimpleTestCase):
+
+    def test_home_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
