db31cb15a83c8aaff5b60573a8bb413a994d32e7
Kennedy Njeri <mistakenz123@gmail.com>
2019-03-03 21:01:32 +0300

    Added home template ok tests

--- mentee/tests.py
+++ mentee/tests.py
@@ -11,3 +11,12 @@ class HomePageTests(SimpleTestCase):
     def test_home_status_code(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
+
+    def test_index_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
