2c1acdae0ee7eb6b589fbc658b6fe9667f1c0c16
Kennedy Njeri <mistakenz123@gmail.com>
2019-04-01 21:36:42 +0300

    approved messages user queryset fix

--- mentee/urls.py
+++ mentee/urls.py
@@ -34,6 +34,12 @@ urlpatterns = [
 
 
 
+
+
+
+
+
+
     #mentor urls
     path('account1/', mentor.AccountView.as_view(), name="account1"),
     path('register1/', mentor.register1, name="register1"),
--- mentee/views/mentee.py
+++ mentee/views/mentee.py
@@ -331,7 +331,6 @@ class Approved(LoginRequiredMixin, UserPassesTestMixin, ListView):
 
 
 
-
 """create new message for a specific user from the profile"""
 class CreateMessageView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin,  CreateView):
 
