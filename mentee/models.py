bba1a70185f093e0790e2365943318d3e242fe75
Kennedy Njeri <mistakenz123@gmail.com>
2019-08-13 11:04:50 +0300

    models.py code clean up

--- mentee/models.py
+++ mentee/models.py
@@ -10,70 +10,56 @@ from django.urls import reverse
 from django.utils.timezone import now
 
 
-
-
-
-
-
-
-
 class User(AbstractUser):
-
-
     is_mentee = models.BooleanField(default=False)
     is_mentor = models.BooleanField(default=False)
-
 
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     interest = models.CharField(max_length=100, null=True)
 
-
-
     def __str__(self):
         return self.user.username
 
 
-
-"""Mentee models"""
 class Mentee(models.Model):
-    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    #interests = models.OneToOneField(Subject, related_name='mentees', on_delete=models.CASCADE, null=True)
+    """Mentee models"""
 
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+
+    # interests = models.OneToOneField(Subject, related_name='mentees', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.user.username
 
 
-"""Mentor models"""
 class Mentor(models.Model):
-    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    #interests = models.OneToOneField(Subject, related_name='mentors', on_delete=models.CASCADE, null=True)
+    """Mentor models"""
 
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+
+    # interests = models.OneToOneField(Subject, related_name='mentors', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.user.username
 
 
-"""Profile Model"""
 class Profile(models.Model):
+    """Profile Model"""
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     education = models.CharField(default='degree', max_length=100)
     registration = models.CharField(default='BBIT/2014/62324', max_length=100)
 
-
     def __str__(self):
         return f'{self.user.username} '
-
-
 
     def save(self, **kwargs):
         super().save()
 
         img = Image.open(self.image.path)
-
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
@@ -90,9 +76,9 @@ class Profile(models.Model):
         instance.profile.save()
 
 
-
-"""Reply Model"""
 class Reply(models.Model):
+    """Reply Model"""
+
     sender = models.ForeignKey(User, related_name="sender2", on_delete=models.CASCADE, null=True)
     reply = models.TextField(blank=True, null=True)
     replied_at = models.DateTimeField(blank=True, null=True)
@@ -101,36 +87,30 @@ class Reply(models.Model):
     def __str__(self):
         return "From {}, in {}".format(self.sender.username, self.conversation)
 
-
     def save(self, *args, **kwargs):
-
         if (self.reply and self.replied_at is None):
-
             self.replied_at = now()
 
         super(Reply, self).save(*args, **kwargs)
 
 
-
-"""Conversation Model"""
-
 class Conversation(models.Model):
+    """Conversation Model"""
 
     sender = models.ForeignKey(User, related_name="sender1", on_delete=models.CASCADE, null=True)
     receipient = models.ForeignKey(User, related_name="receipient1", on_delete=models.CASCADE)
     conversation = models.TextField(max_length=100)
     sent_at = models.DateTimeField(null=True, blank=True)
-    #reply = models.TextField(blank=True, null=True)
-    #replied_at = models.DateTimeField(blank=True, null=True)
+
+    # reply = models.TextField(blank=True, null=True)
+    # replied_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         ordering = ['-sent_at']
 
-
     @property
     def get_replies(self):
         return self.replies.all()
-
 
     def __str__(self):
         return "From {}, to {}".format(self.sender.username, self.receipient.username)
@@ -139,16 +119,15 @@ class Conversation(models.Model):
         if not self.id:
             self.sent_at = timezone.now()
 
-        #if (self.reply and self.replied_at is None):
-            #self.replied_at = now()
+        # if (self.reply and self.replied_at is None):
+        # self.replied_at = now()
 
         super(Conversation, self).save(*args, **kwargs)
 
 
-
-
-"""Message Model"""
 class Msg(models.Model):
+    """Message Model"""
+
     sender = models.ForeignKey(User, related_name="sender", on_delete=models.CASCADE, null=True)
     receipient = models.ForeignKey(User, related_name="receipient", on_delete=models.CASCADE)
     msg_content = models.TextField(max_length=100)
@@ -158,12 +137,8 @@ class Msg(models.Model):
     is_approved = models.BooleanField(default=False, verbose_name="Approve?")
     date_approved = models.DateTimeField(blank=True, null=True)
 
-
-
     def __str__(self):
         return "From {}, to {}".format(self.sender.username, self.receipient.username)
-
-
 
     def save(self, *args, **kwargs):
         if not self.id:
@@ -175,19 +150,7 @@ class Msg(models.Model):
         if (self.comment and self.comment_at is None):
             self.comment_at = now()
 
-
         super(Msg, self).save(*args, **kwargs)
-
-
 
     class Meta:
         ordering = ['-sent_at']
-
-
-
-
-
-
-
-
-
